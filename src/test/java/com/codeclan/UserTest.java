3189aa77de8e494a14ad5d3c1d6956345d4b0239
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-23 17:16:18 +0000

    Add test for getter and setter for User

--- src/test/java/com/codeclan/UserTest.java
+++ src/test/java/com/codeclan/UserTest.java
@@ -1,4 +1,22 @@
 package com.codeclan;
 
+import com.codeclan.models.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
+
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User("Paul");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Paul", user.getUsername());
+    }
 }
